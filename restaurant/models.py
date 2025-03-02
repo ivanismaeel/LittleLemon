738baf9589dfa7d21a78740d950931a6616cb3c8
ivanismaeel <ivanismaeel@gmail.com>
2025-03-02 19:12:03 +0100

    Add Menu and Booking models and migrate to MySQL database

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateField()
 
-# Create your models here.
+    def __str__(self):
+        return self.name
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return self.title
